c3149d38118a78746f42567b87b450da5661f4d1
Henrique Leote <henriqueleote@outlook.com>
2023-06-01 15:40:40 +0200

    Regex update

--- channelBot.py
+++ channelBot.py
@@ -1,7 +1,6 @@
-import os
-
-import psutil as psutil
 from telethon import TelegramClient, events
+import os
+import psutil
 import re
 
 # Austrian number account
@@ -10,11 +9,11 @@ api_hash = 'aed5482cd0ee156892ac0eab0f2bf3f4'
 channel_id = -1001921638321
 client = TelegramClient('my-client', api_id, api_hash)
 chat_list = [-1001314249243, -1001458489616, -1001174207332, -1001395637146, -1001007590845, -1001103963350, -1001493765825, -1001314084172, -1001537106037]
+discount_val = 65
 
 
 def calcDiscount(prices):
     prices.sort()
-    print(prices)
     if prices[0] > prices[1]:
         max_val = prices[0]
         min_val = prices[1]
@@ -24,34 +23,31 @@ def calcDiscount(prices):
     val = float(max_val) - float(min_val)
     val2 = val / float(max_val)
     percentage_discount = format(val2 * 100, '.0f')
-    print(f'% -> {percentage_discount}')
     return float(percentage_discount)
 
 
 def getPercentage(text):
     #pattern = r"(\d{1,2}[.,]?\d{0,2})\s*€"  # Regex pattern to match prices after the Euro symbol
-    pattern = r"(\d{1,3}(?:[.,]\d{3})*(?:[.,]\d{1,2})?)\s*€"
-    matches = re.findall(pattern, text)
-    if matches and len(matches) == 2:
-        prices = [float(match.replace(',', '.')) for match in matches]
-        return calcDiscount(prices)
-    return False
+    pattern = r'(\d{1,3}(?:[.,]\d{3})*(?:[.,]\d+)?)\s?€'
+    prices = re.findall(pattern, text)
+    formatted_prices = [price.replace(',', '') for price in prices]
+    return calcDiscount(formatted_prices)
 
 @client.on(events.NewMessage(chats=chat_list))
 async def my_event_handler(event):
     await event.message.mark_read()
     percentage = getPercentage(event.message.message)
     if (percentage):
-        if(percentage) >= 65:
+        if(percentage) >= discount_val:
             await client.forward_messages(channel_id, event.message)
         else:
-            print(f'{percentage} -> {event.message.message}')
+            print(f'{event.message.message} -> {percentage}% discount')
 
 async def send_start_message():
     channel = await client.get_entity(channel_id)
     print('running...')
     print(psutil.Process(os.getpid()).memory_info().rss / 1024 ** 2)
-    #await client.send_message(channel, "Now redirecting products with discount over 65%!")
+    await client.send_message(channel, f"Now redirecting channel products with discount over {discount_val}%!")
 
 client.start()
 client.loop.run_until_complete(send_start_message())
