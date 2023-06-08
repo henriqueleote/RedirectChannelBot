dc7334aba093b385282231b381ee01bdf9e3d220
Henrique Leote <henriqueleote@outlook.com>
2023-06-08 14:10:34 +0200

    Fixed the multilple dot problem

--- ChannelRedirectBot.py
+++ ChannelRedirectBot.py
@@ -23,11 +23,13 @@ def calcDiscount(prices):
     percentage_discount = format(val2 * 100, '.0f')
     return float(percentage_discount)
 
+# TODO doesnt work with 1.440.00
 # Function to extract prices from text and calculate the percentage discount
 def getPercentage(text):
     pattern = r'(\d{1,3}(?:[.,]\d{3})*(?:[.,]\d+)?)\s?€'
+
     prices = re.findall(pattern, text)
-    formatted_prices = [price.replace(',', '.') for price in prices]
+    formatted_prices = [price.replace('.', '').replace(',', '.') for price in prices]
     print(formatted_prices)
     if len(formatted_prices) == 2:
         return calcDiscount(formatted_prices)
